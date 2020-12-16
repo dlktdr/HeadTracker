c6eddc50e2687045c74051350f8de012260d633b
Cliff Blackburn <cblackburn@powertechrepairs.com>
2020-12-16 10:18:13 -0800

    Create a version 0.3 Hex File

--- firmware/src/DIY_headtracker/Sensors.cpp
+++ firmware/src/DIY_headtracker/Sensors.cpp
@@ -288,12 +288,10 @@ void InitSensors()
   /* Initialise the sensor */
   if(!bno.begin())
   {
-    /* There was a problem detecting the BNO055 ... check your connections */
-    Serial.println("No BNO055 detected... Check your wiring!");
     // Infinate Loop
     while(1) {
-      delay(1000);
-      Serial.println("No BNO055 detected... Check your wiring!");
+      Serial.println("No BNO055 detected... Check your wiring");
+      delay(1000);    
     }
   }
 
