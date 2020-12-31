6dd1ba3e3f069047b5b8c4b9b7049ae845689956
Cliff Blackburn <cblackburn@powertechrepairs.com>
2020-12-30 20:10:09 -0800

    Move thread period settings to .h

--- firmware/src/Nano33BLE/src/sense.h
+++ firmware/src/Nano33BLE/src/sense.h
@@ -1,7 +1,15 @@
 #ifndef SENSE_H
 #define SENSE_H
 
-void sense_Init();
+// Ideal period of filter, not super accurate due low precision on 
+// thread sleep. Can't seem to get faster than this
+const int SLEEPTIME = 7000;
+
+// Runs Filter 5x more than update sensors 
+// 
+const int SENSEUPDATE = 6; 
+
+int sense_Init();
 void sense_Thread();
 float normalize( const float value, const float start, const float end );
 
