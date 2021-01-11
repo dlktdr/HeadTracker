e9eaccf29383d4f73459a5578653cd5a58be7158
Cliff Blackburn <cblackburn@powertechrepairs.com>
2021-01-10 20:35:40 -0800

    Revert "Add setup default pins"
    This reverts commit 1b8d3c07ac01a3b77fb0e35b10876f6a3235ee54.

--- firmware/src/Nano33BLE/src/PPM/PPMOut.cpp
+++ firmware/src/Nano33BLE/src/PPM/PPMOut.cpp
@@ -77,20 +77,16 @@ void PpmOut::setInverted(bool inv)
 void PpmOut::attimeout() {    
     
     // We should have fired early. Find out by how much
-    //uint64_t ex_wait = (dots[current_dot] * 1000) - duration_cast<nanoseconds>(timer.elapsed_time()).count();
-    
-    int64_t ex_wait = static_cast<int64_t>(dots[current_dot]) - static_cast<int64_t>(duration_cast<microseconds>(timer.elapsed_time()).count());
-    timer.reset();
-    
+    uint64_t ex_wait = (dots[current_dot] * 1000) - duration_cast<nanoseconds>(timer.elapsed_time()).count();
     // Start measuring for next pulse
-    
+    timer.reset();
     
 
     digitalWrite(A1,HIGH); // Measure how much time wasted here
     
-    // Pause only when output high, sync pulse can vary without
-    /*if(ex_wait < 100)                
-        wait_us(ex_wait);*/
+    // Pause only when output high, sync pulse can vary without effect
+    if(ex_wait < 30000 && pulse_out == 1)                
+        wait_ns(ex_wait);
 
     digitalWrite(A1,LOW);
 
@@ -98,12 +94,11 @@ void PpmOut::attimeout() {
     ppm = pulse_out;
        
     // Setup next interrupt, fire it early to adjust out some jitter
-    timeout.attach_us(callback(this, &PpmOut::attimeout), dots[current_dot] - JITTER_TIME * 2);
-    
+    timeout.attach_us(callback(this, &PpmOut::attimeout), dots[current_dot] - JITTER_TIME);
     current_dot++;
      
     if(current_dot == channel_number*2+2) { // 2 for FRAME_SYNC
-        current_dot = 0;
+        current_dot = 0;        
     }
     
     
