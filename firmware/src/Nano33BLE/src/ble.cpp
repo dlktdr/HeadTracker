1157bcfaa25dfd9cd47a0ccf4c63a84555cf7d25
Cliff Blackburn <cblackburn@powertechrepairs.com>
2021-01-24 06:45:18 -0800

    Add code to work with Yuri's PARA solution
    Modification to Arduino BLE Library requried!!
    
    This code still intermittently crashes on connection..

--- firmware/src/Nano33BLE/src/ble.cpp
+++ firmware/src/Nano33BLE/src/ble.cpp
@@ -1,3 +1,7 @@
+/* Much of this bluetooth PARA system interface was discovered by Yuri Soldak.
+ *     https://github.com/ysoldak/HeadTracker
+ */
+
 #include <ArduinoBLE.h>
 #include <ArduinoJson.h>
 #include "dataparser.h"
@@ -6,23 +10,33 @@
 #include "serial.h"
 #include "main.h"
 
-BLEDevice peripheral;
-BLEService bleservice("FFF0");
-BLEService bleservice2("180A");
-BLECharacteristic s2c1("2A50", BLERead,10);
+using namespace events;
 
-BLECharacteristic blecar1("FFF1", BLERead | BLEWrite, 60);
-BLECharacteristic blecar2("FFF2", BLERead , 60);
-BLECharacteristic blecar3("FFF3", BLEWriteWithoutResponse, 60);
-BLECharacteristic blecar5("FFF5", BLERead , 60);
-BLECharacteristic blecar6("FFF6", BLENotify | BLEWriteWithoutResponse | BLEWrite, 20);
+uint8_t sysid_data[8] = { 0xF1, 0x63, 0x1B, 0xB0, 0x6F, 0x80, 0x28, 0xFE };
+uint8_t m_data[3] = { 0x41, 0x70, 0x70 };
+uint8_t ieee_data[14] = { 0xFE, 0x00, 0x65, 0x78, 0x70, 0x65, 0x72, 0x69, 0x6D, 0x65, 0x6E, 0x74, 0x61, 0x6C };
+uint8_t pnpid_data[7] = { 0x01, 0x0D, 0x00, 0x00, 0x00, 0x10, 0x01 };
+
+BLEService info("180A");
+BLECharacteristic sysid("2A23", BLERead, 8);
+BLECharacteristic manufacturer("2A29", BLERead, 3);
+BLECharacteristic ieee("2A2A", BLERead, 14);
+BLECharacteristic pnpid("2A50", BLERead, 7);
+
+BLEService para("FFF0");
+BLEByteCharacteristic fff1("FFF1", BLERead | BLEWrite);
+BLEByteCharacteristic fff2("FFF2", BLERead);
+/*BLECharacteristic fff3("FFF3", BLEWriteWithoutResponse, 32);
+BLECharacteristic fff5("FFF5", BLERead, 32);*/
+BLECharacteristic fff6("FFF6", BLEWriteWithoutResponse | BLENotify, 32);
+
 bool bleconnected = false;
 bool oktowrite = true;
 
 //-----------------------------------------------------
 // FROM OPENTX 2.3 - bluetooth.cpp / .h
 
-#define limit(l,v,h) (MAX(MIN( v , h ), l ))
+
 #define LEN_BLUETOOTH_ADDR              16
 #define MAX_BLUETOOTH_DISTANT_ADDR      6
 #define BLUETOOTH_LINE_LENGTH           32
@@ -35,173 +49,150 @@ uint8_t buffer[BLUETOOTH_LINE_LENGTH+1];
 uint8_t bufferIndex = 0;
 uint8_t crc=0;
 
+
+
+int16_t limit(int16_t l, int16_t v, int16_t h) {return MAX(MIN((v),(h)),(l));}
+
 void pushByte(uint8_t byte)
 {
-  crc ^= byte;
-  if (byte == START_STOP || byte == BYTE_STUFF) {
-    buffer[bufferIndex++] = BYTE_STUFF;
-    byte ^= STUFF_MASK;
-  }
-  buffer[bufferIndex++] = byte;
+    crc ^= byte;
+    if (byte == START_STOP || byte == BYTE_STUFF) {
+        buffer[bufferIndex++] = BYTE_STUFF;
+        byte ^= STUFF_MASK;
+    }
+    buffer[bufferIndex++] = byte;
 }
 
 void sendTrainer()
 {
-  int16_t PPM_range = 512*2;  
-  
-  int firstCh = 0;
-  int lastCh = firstCh + 8;
-  
-  // Allocate Channel Mappings, Set Default to all Center
-  uint16_t channelOutputs[24];
-  for(int i=0;i < 24; i++) {
-    channelOutputs[i] = TrackerSettings::DEF_CENTER;
-  }
-  
-  // Read Shared Data
-  dataMutex.lock();
-  uint16_t to,ro,po;
-  trkset.getPPMValues(to,ro,po);
+    // Allocate Channel Mappings, Set Default to all Center
+    uint16_t channelOutputs[12];
+    for(int i=0;i < 12; i++) {
+        channelOutputs[i] = 1500;
+    }
+
+    // Read Shared Data, skip if locked don't wait
+    uint16_t to,ro,po;
+    trkset.getPPMValues(to,ro,po);
+
     // Set Channel Values
-  channelOutputs[trkset.panCh()+firstCh] = po;
-  channelOutputs[trkset.tiltCh()+firstCh] = to;
-  channelOutputs[trkset.rollCh()+firstCh] = ro;
-  dataMutex.unlock();
-  
-  uint8_t * cur = buffer;
-  bufferIndex = 0;
-  crc = 0x00;
+    channelOutputs[trkset.panCh()-1] = po;
+    channelOutputs[trkset.tiltCh()-1] = to;
+    channelOutputs[trkset.rollCh()-1] = ro;
 
-  buffer[bufferIndex++] = START_STOP; // start byte
-  pushByte(0x80); // trainer frame type?
-  for (int channel=0; channel<lastCh; channel+=2, cur+=3) {
-    
-    uint16_t channelValue1 = ((channelOutputs[channel] - TrackerSettings::DEF_CENTER) /2) + TrackerSettings::DEF_CENTER;
-    uint16_t channelValue2 = ((channelOutputs[channel+1] - TrackerSettings::DEF_CENTER) /2) + TrackerSettings::DEF_CENTER;
-    
-    //uint16_t channelValue1 = PpmOut::PPM_CENTER + limit((int16_t)-PPM_range, (int16_t)ppmout->getChannel(channel) - PpmOut::PPM_CENTER, (int16_t)PPM_range) / 2;
-    //uint16_t channelValue2 = PpmOut::PPM_CENTER + limit((int16_t)-PPM_range, (int16_t)ppmout->getChannel(channel+1) - PpmOut::PPM_CENTER, (int16_t)PPM_range) / 2;
-    pushByte(channelValue1 & 0x00ff);
-    pushByte(((channelValue1 & 0x0f00) >> 4) + ((channelValue2 & 0x00f0) >> 4));
-    pushByte(((channelValue2 & 0x000f) << 4) + ((channelValue2 & 0x0f00) >> 8));
-  }
-  
-  buffer[bufferIndex++] = crc;
-  buffer[bufferIndex++] = START_STOP; // end byte
-  
-  
-  // Write to characteristic
-  blecar6.writeValue(buffer,bufferIndex);
-  
+    uint8_t * cur = buffer; 
+    bufferIndex = 0;
+    crc = 0x00;
 
-  bufferIndex = 0;
+    buffer[bufferIndex++] = START_STOP; // start byte
+    pushByte(0x80); // trainer frame type?
+    for (int channel=0; channel<8; channel+=2, cur+=3) {
+        
+        uint16_t channelValue1 = channelOutputs[channel];
+        uint16_t channelValue2 = channelOutputs[channel+1];
+        
+        pushByte(channelValue1 & 0x00ff);
+        pushByte(((channelValue1 & 0x0f00) >> 4) + ((channelValue2 & 0x00f0) >> 4));
+        pushByte(((channelValue2 & 0x000f) << 4) + ((channelValue2 & 0x0f00) >> 8));
+    }
+    
+    buffer[bufferIndex++] = crc;
+    buffer[bufferIndex++] = START_STOP; // end byte
+        
+    fff6.writeValue(buffer,bufferIndex);
+    
+    bufferIndex = 0;
+
 }
 
 //-----------------------------------------------------
 
 
-void blePeripheralConnectHandler(BLEDevice central) {
-  // central connected event handler
-  serialWrite("Connected event, central: ");
-  serialWriteln(central.address().c_str());   
-  bleconnected = true;
+void blePeripheralConnectHandler(BLEDevice central) 
+{
+    //pauseThreads = true;
+    serialWrite("HT: Connected event, Name: ");
+    serialWrite(central.localName().c_str());
+    serialWrite(" Address: ");
+    serialWriteln(central.address().c_str());   
+    
+    if (fff6.subscribe()) {
+        serialWriteln("HT: Subscribed");
+    }
+
+    BLE.stopAdvertise();
+    //pauseThreads = false;
+    bleconnected = true;
 }
 
 void blePeripheralDisconnectHandler(BLEDevice central) 
 {
-  // central disconnected event handler
-  serialWrite("Disconnected event, central: ");
-  serialWriteln(central.address().c_str());
-  bleconnected = false;
-}
+   // pauseThreads = true;
 
-void characteristicRead(BLEDevice central, BLECharacteristic characteristic) 
-{
-  serialWrite("CR "); serialWrite(characteristic.uuid()); serialWrite(" Read ");  
-}
-
-void characteristicWritten(BLEDevice central, BLECharacteristic characteristic) 
-{
-  serialWrite("Characteristic event, written: ");
-
-  char ffer[200];
-  int i= blecar1.valueLength();
-  characteristic.readValue(ffer,i++);
-  ffer[i] = '\0';
-  serialWrite("CR "); serialWrite(characteristic.uuid()); serialWrite(" ");
-  serialWrite(ffer);    serialWrite("\r\n");
+    // Central Disconnected event handler
+    serialWrite("HT: Disconnected event, central: ");
+    serialWriteln(central.address().c_str());
+    
+    BLE.advertise();
+    bleconnected = false;
+    //pauseThreads = false;
 }
 
 void bt_Init() 
 {
-  if(!BLE.begin()) {
-    serialWriteln("Unable to start Bluetooth");    
-  } else {
-    serialWriteln("Started BT");
-    
-
-    bleservice.addCharacteristic(blecar1);
-    bleservice.addCharacteristic(blecar2);
-    bleservice.addCharacteristic(blecar3);
-    bleservice.addCharacteristic(blecar5);
-    bleservice.addCharacteristic(blecar6);
-    bleservice2.addCharacteristic(s2c1);
-
-    BLE.setAdvertisedService(bleservice);
-    BLE.addService(bleservice);
-    BLE.addService(bleservice2);
-    BLE.setLocalName("Head Tracker");
-    
-
-    // assign event handlers for connected, disconnected to peripheral
-    BLE.setEventHandler(BLEConnected, blePeripheralConnectHandler);
-    BLE.setEventHandler(BLEDisconnected, blePeripheralDisconnectHandler);
-    blecar1.setEventHandler(BLEWritten, characteristicWritten);
-    blecar1.setEventHandler(BLERead, characteristicRead);
-    
-    blecar2.setEventHandler(BLEWritten, characteristicWritten);
-    blecar2.setEventHandler(BLERead, characteristicRead);
-    
-    blecar3.setEventHandler(BLEWritten, characteristicWritten);
-    blecar3.setEventHandler(BLERead, characteristicRead);
-    
-    blecar5.setEventHandler(BLEWritten, characteristicWritten);
-    blecar5.setEventHandler(BLERead, characteristicRead);
-    
-    blecar6.setEventHandler(BLEWritten, characteristicWritten);
-    blecar6.setEventHandler(BLERead, characteristicRead);
+    if(!BLE.begin()) {
+        serialWriteln("HT: Unable to start Bluetooth");    
+    } else {
+        serialWriteln("HT: Started BT");
     
     BLE.setConnectable(true);
+    BLE.setLocalName("Hello");
+
+    info.addCharacteristic(sysid);
+    info.addCharacteristic(manufacturer);
+    info.addCharacteristic(ieee);
+    info.addCharacteristic(pnpid);
+
+    BLE.addService(info);
+    sysid.writeValue(sysid_data, 8);
+    manufacturer.writeValue(m_data, 3);
+    ieee.writeValue(ieee_data, 14);
+    pnpid.writeValue(pnpid_data, 7);
+
+    BLE.addService(para);
+    BLE.setAdvertisedService(para);
+    para.addCharacteristic(fff1);
+    para.addCharacteristic(fff2);
+    /*para.addCharacteristic(fff3);
+    para.addCharacteristic(fff5);*/
+    para.addCharacteristic(fff6);
+
+   
+    fff1.writeValue(0x01);
+    fff2.writeValue(0x02);
+
     BLE.advertise();
+
+    trkset.setBLEAddress(BLE.address().c_str());
+
+    BLE.setEventHandler(BLEConnected, blePeripheralConnectHandler);
+    BLE.setEventHandler(BLEDisconnected, blePeripheralDisconnectHandler);
   }
 }
 
 void bt_Thread() {
-  int counter=0;
-  while(1) {    
-
-    if(pauseForEEPROM) {
-      ThisThread::sleep_for(std::chrono::milliseconds(100)); 
-      continue;
+    if(pauseThreads) {
+        queue.call_in(std::chrono::milliseconds(100),bt_Thread);
+        return;
     }
 
     digitalWrite(LEDB,LOW);
-    
-    // Blue tooth connected send the trainer information
-    if(BLE.connected() && counter > 2) { 
-      sendTrainer();
-      counter = 0;
-    } else 
-      counter++;
 
-    // Poll BT and Handle    
-    BLE.poll();   
-        
+    // Connected?
+    if(BLE.central().connected()) 
+        sendTrainer();
+
     digitalWrite(LEDB,HIGH);  
-
-    // Can't Seem to go faster than apx here without crashing the thread???
-    // Caused by the write in sendTrainer to the characteristic.
-    // I assume the last write hasn't been completed but not sure how to check.
-    ThisThread::sleep_for(std::chrono::milliseconds(50)); 
-  }
+    
+    queue.call_in(std::chrono::milliseconds(BT_PERIOD),bt_Thread);
 }
