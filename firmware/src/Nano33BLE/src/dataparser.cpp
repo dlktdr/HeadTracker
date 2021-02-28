5c84fa7e1de307820cfe0062d3642f3d864ef9b0
Cliff B <cblackburn320@gmail.com>
2021-02-28 02:22:34 -0800

    Escape special chars in CRC and whitespace

--- firmware/src/Nano33BLE/src/dataparser.cpp
+++ firmware/src/Nano33BLE/src/dataparser.cpp
@@ -18,9 +18,11 @@ Kernel::Clock::time_point uiResponsive = Kernel::Clock::now();
 bool sendData=false;
 bool uiconnected=false;
 
+uint16_t escapeCRC(uint16_t crc);
+
 // Handles all data transmission with the UI via Serial port
 void data_Thread()
-{     
+{
     if(pauseThreads) {
         queue.call_in(std::chrono::milliseconds(100),data_Thread);
         return;
@@ -28,44 +30,44 @@ void data_Thread()
 
     DynamicJsonDocument json(RX_BUF_SIZE);
 
-    //while(1) {    
+    //while(1) {
     digitalWrite(LEDR,LOW); // Serial RX Blue, Off
 
     // Check if data is ready
-    while(buffersFilled() > 0) {
+    while(buffersFilled()) {
         char *buffer = getJSONBuffer();
 
         // CRC Check Data
-        int len = strlen(buffer);
+         int len = strlen(buffer);
         if(len > 2) {
-            uint16_t calccrc = uCRC16Lib::calculate(buffer,len-sizeof(uint16_t));
+            uint16_t calccrc = escapeCRC(uCRC16Lib::calculate(buffer,len-sizeof(uint16_t)));
             if(calccrc != *(uint16_t*)(buffer+len-sizeof(uint16_t))) {
-                serialWrite("\x15\r\n"); // Not-Acknowledged      
-                continue;
+                serialWrite("\x15\r\n"); // Not-Acknowledged
+                break;
             } else {
                 serialWrite("\x06\r\n"); // Acknowledged
             }
             // Remove CRC from end of buffer
             buffer[len-sizeof(uint16_t)] = 0;
-        }
 
-        DeserializationError de = deserializeJson(json, buffer);
-        if(de) {
-            if(de == DeserializationError::IncompleteInput)
-                serialWrite("HT: DeserializeJson() Failed - IncompleteInput\r\n");
-            else if(de == DeserializationError::InvalidInput)
-                serialWrite("HT: DeserializeJson() Failed - InvalidInput\r\n");
-            else if(de == DeserializationError::NoMemory)
-                serialWrite("HT: DeserializeJson() Failed - NoMemory\r\n");
-            else if(de == DeserializationError::NotSupported)
-                serialWrite("HT: DeserializeJson() Failed - NotSupported\r\n");
-            else if(de == DeserializationError::TooDeep)
-                serialWrite("HT: DeserializeJson() Failed - TooDeep\r\n");
-            else 
-                serialWrite("HT: DeserializeJson() Failed - Other\r\n");
-        } else {
-            // Parse The JSON Data
-            parseData(json);            
+            DeserializationError de = deserializeJson(json, buffer);
+            if(de) {
+                if(de == DeserializationError::IncompleteInput)
+                    serialWrite("HT: DeserializeJson() Failed - Incomplete Input\r\n");
+                else if(de == DeserializationError::InvalidInput)
+                    serialWrite("HT: DeserializeJson() Failed - Invalid Input\r\n");
+                else if(de == DeserializationError::NoMemory)
+                    serialWrite("HT: DeserializeJson() Failed - NoMemory\r\n");
+                else if(de == DeserializationError::NotSupported)
+                    serialWrite("HT: DeserializeJson() Failed - NotSupported\r\n");
+                else if(de == DeserializationError::TooDeep)
+                    serialWrite("HT: DeserializeJson() Failed - TooDeep\r\n");
+                else
+                    serialWrite("HT: DeserializeJson() Failed - Other\r\n");
+            } else {
+                // Parse The JSON Data
+                parseData(json);
+            }
         }
     }
 
@@ -95,27 +97,27 @@ void data_Thread()
     if(uiResponsive > curtime) {
         uiconnected = true;
         // Build JSON of Data
-        json.clear();      
+        json.clear();
         dataMutex.lock();
         trkset.setJSONData(json);
         dataMutex.unlock();
 
         // Add the Command
-        json["Cmd"] = "Data";               
+        json["Cmd"] = "Data";
 
         serialWriteJSON(json);
-    }  else 
+    }  else
         uiconnected = false;
 
 
     digitalWrite(LEDR,HIGH); // Serial RX Blue, Off
- 
+
     queue.call_in(std::chrono::milliseconds(DATA_PERIOD),data_Thread);
 }
 
 // New JSON data received from the PC
 void parseData(DynamicJsonDocument &json)
-{    
+{
     JsonVariant v = json["Cmd"];
     if(v.isNull()) {
       serialWrite("HT: Invalid JSON, No Command\r\n");
@@ -124,27 +126,27 @@ void parseData(DynamicJsonDocument &json)
 
     // For strcmp;
     const char *command = v;
-    
+
     // Reset Center
-    if(strcmp(command,"RstCnt") == 0) { 
+    if(strcmp(command,"RstCnt") == 0) {
         serialWrite("HT: Resetting Center\r\n");
         pressButton();
         uiResponsive = Kernel::Clock::now() + std::chrono::milliseconds(UIRESPONSIVE_TIME);
 
     // Settings Sent from UI
-    } else if (strcmp(command, "Setttings") == 0) {        
+    } else if (strcmp(command, "Setttings") == 0) {
         trkset.loadJSONSettings(json);
-        serialWrite("HT: Saving Settings\r\n");           
+        serialWrite("HT: Saving Settings\r\n");
         uiResponsive = Kernel::Clock::now() + std::chrono::milliseconds(UIRESPONSIVE_TIME);
 
     // Save to Flash
     } else if (strcmp(command, "Flash") == 0) {
-        serialWrite("HT: Saving Settings\r\n");           
+        serialWrite("HT: Saving Settings\r\n");
         trkset.saveToEEPROM();
         uiResponsive = Kernel::Clock::now() + std::chrono::milliseconds(UIRESPONSIVE_TIME);
 
     // Get settings
-    } else if (strcmp(command, "GetSet") == 0) {  
+    } else if (strcmp(command, "GetSet") == 0) {
         serialWrite("HT: Sending Settings\r\n");
         json.clear();
         trkset.setJSONSettings(json);
@@ -172,6 +174,28 @@ void parseData(DynamicJsonDocument &json)
 
     // Unknown Command
     } else {
-        serialWrite("HT: Unknown Command\r\n");    
+        serialWrite("HT: Unknown Command\r\n");
     }
+}
+
+// Remove any of the escape characters
+
+uint16_t escapeCRC(uint16_t crc)
+{
+    // Characters to escape out
+    uint8_t crclow = crc & 0xFF;
+    uint8_t crchigh = (crc >> 8) & 0xFF;
+    if(crclow == 0x00 ||
+       crclow == 0x02 ||
+       crclow == 0x03 ||
+       crclow == 0x06 ||
+       crclow == 0x15)
+        crclow ^= 0xFF; //?? why not..
+    if(crchigh == 0x00 ||
+       crchigh == 0x02 ||
+       crchigh == 0x03 ||
+       crchigh == 0x06 ||
+       crchigh == 0x15)
+        crchigh ^= 0xFF; //?? why not..
+    return (uint16_t)crclow | ((uint16_t)crchigh << 8);
 }
