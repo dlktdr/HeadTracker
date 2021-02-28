906d24a4114750ca33a5695892d4c47f3edbf70d
Cliff B <cblackburn320@gmail.com>
2021-02-28 02:22:11 -0800

    Buffer issue and whitespace

--- firmware/src/Nano33BLE/src/serial.cpp
+++ firmware/src/Nano33BLE/src/serial.cpp
@@ -26,13 +26,13 @@ volatile int bufsUsed=0;
 // Buffered write to serial
 Mutex serWriteMutex;
 
-void serial_Init() 
+void serial_Init()
 {
-    Serial.begin(115200); // Baud doesn't actually do anything with serial CDC    
+    Serial.begin(115200); // Baud doesn't actually do anything with serial CDC
 }
 
 void serial_Thread()
-{  
+{
     if(pauseThreads) {
         queue.call_in(std::chrono::milliseconds(100),serial_Thread);
         return;
@@ -48,61 +48,62 @@ void serial_Thread()
     // probably have to look at the nrf datasheet.
 
     for(int i =0;i<bytx;i++) {
-      serout.pop(txa[i]);      
+      serout.pop(txa[i]);
     }
     Serial.write(txa,bytx); // !!!! ONLY PLACE SERIAL.WRITE SHOULD BE USED !!!!
     digitalWrite(LEDG,HIGH); // Serial RX Green, ON
-    queue.call_in(std::chrono::milliseconds(SERIAL_PERIOD),serial_Thread);  
+    queue.call_in(std::chrono::milliseconds(SERIAL_PERIOD),serial_Thread);
 }
 
 // Pop a JSON item off the buffer
-char *getJSONBuffer() 
-{    
-    __disable_irq();    
+char *getJSONBuffer()
+{
+    __disable_irq();
     int bi = bufIndex;
     int bu = bufsUsed;
-    bufsUsed--;    
+    bufsUsed--;
+    __enable_irq();
+
     int i = bi - bu;
     while (i >= RX_BUFFERS)
         i = i - RX_BUFFERS;
     while (i < 0)
         i = i + RX_BUFFERS;
-    __enable_irq();
     return jsondatabuf[i];
 }
 
 // Buffers Used
-int buffersFilled() 
-{    
+int buffersFilled()
+{
     return bufsUsed;
 }
 
 // Serial RX Interrupt, Stay Fast as possible Here,
 void serialrx_Int()
-{  
+{
     digitalWrite(LEDG,LOW); // Serial RX Green, ON
-  
-    int bytes = Serial.available();   
 
-    // Read all available data from Serial  
+    int bytes = Serial.available();
+
+    // Read all available data from Serial
     for(int i=0; i < bytes; i++) {
         char sc = Serial.read();
         if(sc == 0x02) {  // Start Of Text Character, clear buffer
             serin.reset();
 
         } else if (sc == 0x03) { // End of Text Characher, parse JSON data
-        
-            // All buffers filled? Ditch this message                
-            if(bufsUsed == RX_BUFFERS)   {
+
+            // All buffers filled? Ditch this message
+            if(bufsUsed >= RX_BUFFERS-1)   {
                 JSONfault = true;
                 serin.reset();
 
             // Good to store to buffer
-            } else  { 
+            } else  {
                 // Move from the circular buffer into character String
                 int bsz = serin.size();
                 char *dataptr = jsondatabuf[bufIndex];
-                for(int i=0; i < bsz; i++) 
+                for(int i=0; i < bsz; i++)
                     serin.pop(*(dataptr++));
                 *dataptr = 0;
 
@@ -110,7 +111,7 @@ void serialrx_Int()
                 bufsUsed++;
             }
 
-        serin.reset();      
+            serin.reset();
         }
 
         else { // Add data to buffer
@@ -120,12 +121,12 @@ void serialrx_Int()
                 BuffToSmall = true;
                 serin.reset();
             }
-            // 
+            //
             serin.push(sc);
             if(serin.full())
-                SerBufOverflow = true;      
+                SerBufOverflow = true;
         }
-    }      
+    }
     digitalWrite(LEDG,HIGH);  // Serial RX Green, OFF
 }
 
@@ -142,7 +143,7 @@ void serialWrite(float f)
 }
 
 void serialWriteln(char const *data)
-{  
+{
   int br = strlen(data);
   // Append Output to the serial output buffer
   serWriteMutex.lock();
@@ -154,8 +155,8 @@ void serialWriteln(char const *data)
   serWriteMutex.unlock();
 }
 
-void serialWrite(int val) 
-{ 
+void serialWrite(int val)
+{
   char buf[50];
   itoa(val,buf,10);
   int len = strlen(buf);
@@ -177,7 +178,7 @@ void serialWrite(char *data,int len) {
 }
 
 void serialWrite(char const *data) {
-  
+
   int br = strlen(data);
   // Append Output to the serial output buffer
   serWriteMutex.lock();
@@ -194,9 +195,9 @@ void serialWrite(char c) {
 }
 
 void serialWriteJSON(DynamicJsonDocument &json)
-{  
-  serWriteMutex.lock(); 
-  
+{
+  serWriteMutex.lock();
+
   char data[TX_BUF_SIZE];
   int br = serializeJson(json, data, TX_BUF_SIZE); // Serialize, with max 500 char buffer
 
@@ -208,6 +209,6 @@ void serialWriteJSON(DynamicJsonDocument &json)
   serout.push(0x03);
   serout.push('\r');
   serout.push('\n');
-  
-  serWriteMutex.unlock();  
+
+  serWriteMutex.unlock();
 }
