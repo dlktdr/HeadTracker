888547cb83c1c79440bdbd9b86213f10956ab35f
Cliff B <cblackburn320@gmail.com>
2021-03-15 02:33:47 -0700

    Disable incoming CRC, reduce transmission by a few characters

--- gui/src/boardnano33ble.cpp
+++ gui/src/boardnano33ble.cpp
@@ -20,18 +20,21 @@ void BoardNano33BLE::dataIn(QByteArray &data)
 {
     // Found a SOT & EOT Character. JSON Data was sent
     if(data.left(1)[0] == (char)0x02 && data.right(1)[0] == (char)0x03) { // JSON Data
-        QByteArray stripped = data.mid(1,data.length()-2);
+        QByteArray crcs = data.mid(data.length()-3,2);
+        //uint16_t crc = crcs[0] << 8 | crcs[1];
 
-        // *** TODO - Add CRC check here, issue a new settings request.
-        // Increment RXerror counter and fault out if too many errors.
-
-        parseIncomingJSON(QJsonDocument::fromJson(stripped).object().toVariantMap());
+        QByteArray stripped = data.mid(1,data.length()-4);
+        //uint crc2 = escapeCRC(uCRC16Lib::calculate(stripped.data(), stripped.length()));
+        /*if(crc != crc2) {
+            qDebug() << "CRC Fault" << crc << crc2;
+        } else {*/
+            parseIncomingJSON(QJsonDocument::fromJson(stripped).object().toVariantMap());
+        //}
 
         //  Found the acknowldege Character, data was received without error
     } else if(data.left(1)[0] == (char)0x06) {
         // Clear the fault counter
         jsonfaults = 0;
-        emit paramSendComplete();
 
         // If more data in queue, send it and wait for another ack char.
         if(!jsonqueue.isEmpty()) {
@@ -82,7 +85,7 @@ void BoardNano33BLE::saveToRAM()
 
     // Send Changed Data
     emit paramSendStart();
-    sendSerialJSON("Setttings", d2s);
+    sendSerialJSON("Set", d2s);
 
     // Set the data is now matched on the device
     trkset->setDataMatched();
@@ -103,6 +106,7 @@ void BoardNano33BLE::saveToNVM()
 
 void BoardNano33BLE::requestParameters()
 {
+    qDebug() << "JSON Data" << jsonqueue.length();
     if(rxparamfaults == 0) {
         emit paramReceiveStart();
     } else if (rxparamfaults > 3) {
@@ -110,18 +114,24 @@ void BoardNano33BLE::requestParameters()
         return;
     }
 
-    sendSerialJSON("GetSet"); // Get the Settings
+    sendSerialJSON("Get"); // Get the Settings
 
     rxParamsTimer.stop();
-    rxParamsTimer.start(500); // Start a timer, if we haven't got them, try again
+    rxParamsTimer.start(800); // Start a timer, if we haven't got them, try again
 }
 
 // Timer if parameters are not received in time
 void BoardNano33BLE::rxParamsTimeout()
 {
-    // Increment fault counter
-    rxparamfaults++;
-    requestParameters();
+    // Don't increment counter if data hasn't been sent yet
+    if(serialDataOut.size() != 0) {
+        rxParamsTimer.stop();
+        rxParamsTimer.start(500); // Start a timer, if we haven't got them, try again
+        return;
+    } else {
+        rxparamfaults++;
+        requestParameters();
+    }
 }
 
 
@@ -179,6 +189,7 @@ void BoardNano33BLE::sendSerialJSON(QString command, QVariantMap map)
     // to be sent later
     if(jsonfaults != 0) {
         jsonqueue.enqueue(lastjson);
+        emit serialTxReady();
         return;
     }
 
@@ -197,7 +208,7 @@ void BoardNano33BLE::sendSerialJSON(QString command, QVariantMap map)
 void BoardNano33BLE::parseIncomingJSON(const QVariantMap &map)
 {
     // Settings from the Tracker Sent, save them and update the UI
-    if(map["Cmd"].toString() == "Settings") {
+    if(map["Cmd"].toString() == "Set") {
         rxParamsTimer.stop(); // Stop error timer
         rxparamfaults = 0;
         trkset->setAllData(map);
