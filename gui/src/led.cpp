43a9648e18af50d209f8620b4f58f667551d63d5
Cliff B <cblackburn320@gmail.com>
2021-04-24 13:49:03 -0700

    Add Blink Ability to LED

--- gui/src/led.cpp
+++ gui/src/led.cpp
@@ -2,13 +2,39 @@
 
 Led::Led(QWidget *parent) : QWidget(parent)
 {    
-    colon =Qt::green;
+    blink = false;
+    state = false;
+    blinkstate = false;
+    colon = Qt::green;
     coloff = Qt::red;
+    colblink = QColor(251, 255, 0);
     color = coloff;
+    connect(&blinktmr,SIGNAL(timeout()),this,SLOT(blinkTimeout()));
+    blinktmr.setInterval(250);
+    blinktmr.start();
 }
+
+void Led::blinkTimeout()
+{
+    // If state on, never blink
+    if(state) {
+        color = colon;
+        return;
+    }
+
+    // If state off blink
+    blinkstate = !blinkstate;
+    if(blink && blinkstate)
+        color = colblink;
+    else
+        color = coloff;
+    update();
+}
+
 
 void Led::setState(bool a)
 {
+    state = a;
     if(a)
         color = colon;
     else
@@ -23,7 +49,7 @@ void Led::paintEvent(QPaintEvent *event)
 
     QRadialGradient gm(width()/2,height()/2,width()/2);
     gm.setColorAt(0,color);
-    gm.setColorAt(1,color.darker(160));
+    gm.setColorAt(1,color.darker(120));
     painter.setRenderHint(QPainter::Antialiasing, true);
     painter.setBrush(QBrush(gm));
     painter.setPen(QPen(Qt::black));
--- gui/src/led.h
+++ gui/src/led.h
@@ -3,18 +3,28 @@
 
 #include <QWidget>
 #include <QPainter>
+#include <QTimer>
 
 class Led : public QWidget
 {
     Q_OBJECT
 private:
-    QColor color,colon,coloff;
+    QColor color,colon,coloff,colblink;
+    QTimer blinktmr;
+    bool blink;
+    bool blinkstate;
+    bool state;
+
+private slots:
+    void blinkTimeout();
 
 public:
     explicit Led(QWidget *parent = nullptr);
     void setState(bool);
+    void setBlink(bool b) {blink = b;}
     void setOnColor(QColor a) {colon = a;}
     void setOffColor(QColor a) {coloff = a;}
+    void setBlinkolor(QColor a) {colblink = a;}
 signals:
 
 protected:
