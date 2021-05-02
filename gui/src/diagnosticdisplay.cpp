1943b889e591302be22da873f1bf03640d3d1e70
Cliff B <cblackburn320@gmail.com>
2021-05-02 11:21:03 -0700

    Separate Live/Settings

--- gui/src/diagnosticdisplay.cpp
+++ gui/src/diagnosticdisplay.cpp
@@ -7,13 +7,16 @@ DiagnosticDisplay::DiagnosticDisplay(TrackerSettings *ts, QWidget *parent) :
     trkset(ts)
 {
     ui->setupUi(this);
-    connect(trkset,&TrackerSettings::rawGyroChanged,this,&DiagnosticDisplay::updated);
-    connect(trkset,&TrackerSettings::rawMagChanged,this,&DiagnosticDisplay::updated);
+    connect(trkset,&TrackerSettings::liveDataChanged,this,&DiagnosticDisplay::updated);
     updated();
     ui->tblParams->verticalHeader()->setVisible(false);
     ui->tblParams->horizontalHeader()->setVisible(false);
     ui->tblParams->horizontalHeader()->setStretchLastSection(true);
     ui->tblParams->setColumnWidth(0,100);
+    ui->tblLiveData->verticalHeader()->setVisible(false);
+    ui->tblLiveData->horizontalHeader()->setVisible(false);
+    ui->tblLiveData->horizontalHeader()->setStretchLastSection(true);
+    ui->tblLiveData->setColumnWidth(0,100);
 }
 
 DiagnosticDisplay::~DiagnosticDisplay()
@@ -28,7 +31,11 @@ void DiagnosticDisplay::updated()
 
     ui->tblParams->clear();
     ui->tblParams->setColumnCount(2);
-    ui->tblParams->setRowCount(livedata.count()+settings.count());
+    ui->tblParams->setRowCount(settings.count());
+    ui->tblLiveData->clear();
+    ui->tblLiveData->setColumnCount(2);
+    ui->tblLiveData->setRowCount(livedata.count());
+
     int row=0;
     QMapIterator<QString, QVariant> i(livedata);
     while (i.hasNext()) {
@@ -39,10 +46,10 @@ void DiagnosticDisplay::updated()
             key->setBackground(QBrush(Qt::lightGray));
             value->setBackground(QBrush(Qt::lightGray));
         }
-        ui->tblParams->setItem(row,0,key);
-        ui->tblParams->setItem(row++,1,value);
+        ui->tblLiveData->setItem(row,0,key);
+        ui->tblLiveData->setItem(row++,1,value);
     }
-
+    row=0;
     QMapIterator<QString, QVariant> x(settings);
     while (x.hasNext()) {
         x.next();
