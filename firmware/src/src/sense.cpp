9d5353aba63557fad5925a7178bb507b0d89e2b1
Cliff B <cblackburn320@gmail.com>
2021-07-19 10:35:15 -0700

    Add RSSI.. Not fully functional. yet

--- firmware/src/src/sense.cpp
+++ firmware/src/src/sense.cpp
@@ -625,8 +625,5 @@ void buildAuxData()
     auxdata[4] = (accy / 2.0f) * pwmrange + TrackerSettings::PPM_CENTER;
     auxdata[5] = (accz / 1.0f) * pwmrange + TrackerSettings::PPM_CENTER;
     auxdata[6] = ((accz -1.0f) / 2.0f) * pwmrange + TrackerSettings::PPM_CENTER;
-    auxdata[6] = TrackerSettings::MIN_PWM;
-
-    // *** FIXME
-  //  auxdata[7] = static_cast<float>(BLE.central().rssi()) / 127.0 * pwmrange + TrackerSettings::MIN_PWM;
+    auxdata[7] = static_cast<float>(BTGetRSSI()) / 127.0 * pwmrange + TrackerSettings::MIN_PWM;
 }
