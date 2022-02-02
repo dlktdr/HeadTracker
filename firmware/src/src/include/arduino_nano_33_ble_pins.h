6f001a2babafef228f787d2e4550508c1138c895
dlktdr <cblackburn320@gmail.com>
2022-02-01 23:35:28 -0800

    Fix incorrect pin numbers

--- firmware/src/src/include/arduino_nano_33_ble_pins.h
+++ firmware/src/src/include/arduino_nano_33_ble_pins.h
@@ -32,12 +32,12 @@
 #define ARDUINO_D12_MISO (32 * 1 + 8)                   /* P1.8 */
 #define ARDUINO_D13_SCK (32 * 0 + 13)                   /* P0.13 */
 
-#define ARDUINO_A0 (32 * 0 + 24)                        /* P0.24 */
+#define ARDUINO_A0 (32 * 0 + 2)                         /* P0.2 */
 #define ARDUINO_A1 (32 * 0 + 5)                         /* P0.5 */
 #define ARDUINO_A2 (32 * 0 + 30)                        /* P0.30 */
 #define ARDUINO_A3 (32 * 0 + 29)                        /* P0.29 */
 #define ARDUINO_A4_SDA (32 * 0 + 31)                    /* P0.31 */
-#define ARDUINO_A5_SCL (32 * 0 + 2)                     /* P0.2 */
+#define ARDUINO_A5_SCL (32 * 0 + 29)                     /* P0.29 */
 #define ARDUINO_A4 ARDUINO_A4_SDA
 #define ARDUINO_A5 ARDUINO_A5_SCL
 #define ARDUINO_A6 (32 * 0 + 28)                        /* P0.28 */
@@ -62,10 +62,6 @@
 #define LED_B ARDUINO_LEDB
 #define LED_G ARDUINO_LEDG
 #define LED_R ARDUINO_LEDR
-
-#define LEDB ARDUINO_LEDB
-#define LEDG ARDUINO_LEDG
-#define LEDR ARDUINO_LEDR
 
 #define HIGH 1
 #define LOW 0
