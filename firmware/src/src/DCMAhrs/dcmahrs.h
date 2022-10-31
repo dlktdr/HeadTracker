20689a517409edc87d1abd67adccc94cc83f04a0
dlktdr <cblackburn320@gmail.com>
2022-10-31 12:57:38 -0700

    Add DCM Ahrs

---
+++ firmware/src/src/DCMAhrs/dcmahrs.h
@@ -0,0 +1,9 @@
+#pragma once
+
+void DcmAHRSInitialize(float U0[3], float U1[3], float U2[3]);
+void DcmAhrsResetCenter();
+void DcmCalculate(float U0[3], float U1[3], float U2[3], float deltat);
+float DcmGetTilt();
+float DcmGetRoll();
+float DcmGetPan();
+void DCMnormalization_Thread();
