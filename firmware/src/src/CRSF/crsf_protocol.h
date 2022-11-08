4b4aa7ac57ff12f9f8eb127f4ebcffb858374295
dlktdr <cblackburn320@gmail.com>
2022-11-08 00:06:23 -0800

    Update crsf_protocol.h

--- firmware/src/src/CRSF/crsf_protocol.h
+++ firmware/src/src/CRSF/crsf_protocol.h
@@ -72,15 +72,41 @@ enum {
     CRSF_FRAME_RX_MSP_FRAME_SIZE = 8,
     CRSF_FRAME_ORIGIN_DEST_SIZE = 2,
 };
-  CRSF_FRAMETYPE_PARAMETER_SETTINGS_ENTRY = 0x2B,
-  CRSF_FRAMETYPE_PARAMETER_READ = 0x2C,
-  CRSF_FRAMETYPE_PARAMETER_WRITE = 0x2D,
-  CRSF_FRAMETYPE_COMMAND = 0x32,
-  // MSP commands
-  CRSF_FRAMETYPE_MSP_REQ = 0x7A,   // response request using msp sequence as command
-  CRSF_FRAMETYPE_MSP_RESP = 0x7B,  // reply with 58 byte chunked binary
-  CRSF_FRAMETYPE_MSP_WRITE = 0x7C,  // write with 8 byte chunked binary (OpenTX outbound telemetry buffer limit)
-} crsf_frame_type_e;
+
+typedef enum {
+    CRSF_FRAMETYPE_GPS = 0x02,
+    CRSF_FRAMETYPE_BATTERY_SENSOR = 0x08,
+    CRSF_FRAMETYPE_HEARTBEAT = 0x0B,
+    CRSF_FRAMETYPE_LINK_STATISTICS = 0x14,
+    CRSF_FRAMETYPE_RC_CHANNELS_PACKED = 0x16,
+    CRSF_FRAMETYPE_SUBSET_RC_CHANNELS_PACKED = 0x17,
+    CRSF_FRAMETYPE_LINK_STATISTICS_RX = 0x1C,
+    CRSF_FRAMETYPE_LINK_STATISTICS_TX = 0x1D,
+    CRSF_FRAMETYPE_ATTITUDE = 0x1E,
+    CRSF_FRAMETYPE_FLIGHT_MODE = 0x21,
+    // Extended Header Frames, range: 0x28 to 0x96
+    CRSF_FRAMETYPE_DEVICE_PING = 0x28,
+    CRSF_FRAMETYPE_DEVICE_INFO = 0x29,
+    CRSF_FRAMETYPE_PARAMETER_SETTINGS_ENTRY = 0x2B,
+    CRSF_FRAMETYPE_PARAMETER_READ = 0x2C,
+    CRSF_FRAMETYPE_PARAMETER_WRITE = 0x2D,
+    CRSF_FRAMETYPE_COMMAND = 0x32,
+    // MSP commands
+    CRSF_FRAMETYPE_MSP_REQ = 0x7A,   // response request using msp sequence as command
+    CRSF_FRAMETYPE_MSP_RESP = 0x7B,  // reply with 58 byte chunked binary
+    CRSF_FRAMETYPE_MSP_WRITE = 0x7C,  // write with 8 byte chunked binary (OpenTX outbound telemetry buffer limit)
+    CRSF_FRAMETYPE_DISPLAYPORT_CMD = 0x7D, // displayport control command
+} crsfFrameType_e;
+
+enum {
+    CRSF_FRAME_GPS_PAYLOAD_SIZE = 15,
+    CRSF_FRAME_BATTERY_SENSOR_PAYLOAD_SIZE = 8,
+    CRSF_FRAME_HEARTBEAT_PAYLOAD_SIZE = 2,
+    CRSF_FRAME_LINK_STATISTICS_PAYLOAD_SIZE = 10,
+    CRSF_FRAME_LINK_STATISTICS_TX_PAYLOAD_SIZE = 6,
+    CRSF_FRAME_RC_CHANNELS_PAYLOAD_SIZE = 22, // 11 bits per channel * 16 channels = 22 bytes.
+    CRSF_FRAME_ATTITUDE_PAYLOAD_SIZE = 6,
+};
 
 typedef enum
 {
