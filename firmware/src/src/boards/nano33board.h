466f54234039028cac85c0789c9beabda51feeb2
dlktdr <cblackburn320@gmail.com>
2023-01-17 14:28:25 -0800

    Found values good for the DTQ_Sysboard

--- firmware/src/src/boards/nano33board.h
+++ firmware/src/src/boards/nano33board.h
@@ -91,5 +91,5 @@ const int dpintoenum[] = {0,  0, IO_D2, IO_D3, IO_D4, IO_D5, IO_D6, IO_D7, IO_D8
 #define D_TO_ENUM(x) (dpintoenum[x])
 
 // Values below were determined by plotting Gyro Output (See sense.cpp, gyroCalibration())
-#define GYRO_STABLE_DIFF 200
-#define ACC_STABLE_DIFF 2
+#define GYRO_STABLE_DIFF 200.0f
+#define ACC_STABLE_DIFF 2.5f
