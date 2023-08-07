74cc4c62ff5eae9826ee950142896444f158bf80
dlktdr <cblackburn320@gmail.com>
2023-08-07 01:32:42 -0700

    Disable many items

--- firmware/src/src/nano33ble.cpp
+++ firmware/src/src/nano33ble.cpp
@@ -23,6 +23,9 @@ K_SEM_DEFINE(saveToFlash_sem, 0, 1);
 
 void start(void)
 {
+
+  printk("Booting\n");
+
   // Initalize IO
   io_init();
 
@@ -46,7 +49,7 @@ void start(void)
 #endif
 
   // Load settings from flash - trackersettings.cpp
-  trkset.loadFromEEPROM();
+  //trkset.loadFromEEPROM();
 
   // Check if center button is held down, force BT Configuration mode
   if (readCenterButton()) {
@@ -71,14 +74,9 @@ void start(void)
 
 // Threads
 K_THREAD_DEFINE(io_Thread_id, 512, io_Thread, NULL, NULL, NULL, IO_THREAD_PRIO, 0, 0);
-K_THREAD_DEFINE(serial_Thread_id, 4096, serial_Thread, NULL, NULL, NULL, SERIAL_THREAD_PRIO,
-                K_FP_REGS, 1000);
-K_THREAD_DEFINE(bt_Thread_id, 1024, bt_Thread, NULL, NULL, NULL, BT_THREAD_PRIO, 0, 0);
-K_THREAD_DEFINE(sensor_Thread_id, 1024, sensor_Thread, NULL, NULL, NULL, SENSOR_THREAD_PRIO,
-                K_FP_REGS, 500);
-K_THREAD_DEFINE(calculate_Thread_id, 1024, calculate_Thread, NULL, NULL, NULL,
-                CALCULATE_THREAD_PRIO, K_FP_REGS, 1000);
-K_THREAD_DEFINE(uartTx_Thread_ID, 1024, uartTx_Thread, NULL, NULL, NULL, UARTTX_THREAD_PRIO, 0,
-                1000);
-K_THREAD_DEFINE(uartRx_Thread_ID, 512, uartRx_Thread, NULL, NULL, NULL, UARTRX_THREAD_PRIO, 0,
-                1000);
+K_THREAD_DEFINE(serial_Thread_id, 4096, serial_Thread, NULL, NULL, NULL, SERIAL_THREAD_PRIO, K_FP_REGS, 1000);
+//K_THREAD_DEFINE(bt_Thread_id, 1024, bt_Thread, NULL, NULL, NULL, BT_THREAD_PRIO, 0, 0);
+K_THREAD_DEFINE(sensor_Thread_id, 1024, sensor_Thread, NULL, NULL, NULL, SENSOR_THREAD_PRIO,                K_FP_REGS, 500);
+K_THREAD_DEFINE(calculate_Thread_id, 1024, calculate_Thread, NULL, NULL, NULL,                CALCULATE_THREAD_PRIO, K_FP_REGS, 1000);
+//K_THREAD_DEFINE(uartTx_Thread_ID, 1024, uartTx_Thread, NULL, NULL, NULL, UARTTX_THREAD_PRIO, 0,                1000);
+//K_THREAD_DEFINE(uartRx_Thread_ID, 512, uartRx_Thread, NULL, NULL, NULL, UARTRX_THREAD_PRIO, 0,                1000);
